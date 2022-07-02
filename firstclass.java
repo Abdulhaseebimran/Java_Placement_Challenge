2d3173cbc3e8b0e0084dfe373955476313fbc754
Abdulhaseebimran <haseebimran0001@gamil.com>
2022-07-02 20:58:55 +0500

    first commit

---
+++ firstclass.java
@@ -0,0 +1,10 @@
+/**
+ * firstclass
+ */
+public class firstclass {
+    // this is  my first code in java
+     public static void main(String[] args) {
+       System.out.println("Hello World"); 
+       System.out.println("Hello Everyone My name is Abdul haseeb and I learned Java");
+    }
+ }
