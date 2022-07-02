97b43ef51b251d6c97d6ecd72d27fc13bcc9f5a4
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-02 21:03:49 +0500

    first commit

---
+++ output.java
@@ -0,0 +1,7 @@
+public class output {
+    public static void main (String[] args){
+        System.out.print("Hello \n");
+        System.out.print("Hello Everyone \n");
+        System.out.print("Hi \n");
+    }
+}
