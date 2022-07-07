f6fab40a360e8e2c90dd0d0f67e485e33a800a09
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-07 19:27:41 +0500

    first commit

---
+++ pattern14.java
@@ -0,0 +1,28 @@
+public class pattern14 {
+    public static void main(String[] args) {
+        int n = 4;
+
+       for(int i=1; i<=n; i++){
+        // spaces
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        // upper half
+        for(int j=1; j<=2*i-1; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+       for(int i=n; i>=1; i--){
+        // spaces
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        // upper half
+        for(int j=1; j<=2*i-1; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
+}
