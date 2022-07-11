6c0b26929d53deba5d9325f2e38a78fe1e3f0f35
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 20:57:33 +0500

    first commit

---
+++ string4.java
@@ -0,0 +1,7 @@
+public class string4 {
+    public static void main(String[] args) {
+        String sentences = "My name is Haseeb";
+        String name = sentences.substring(11); // substring will take beg index , end index 
+        System.out.println(name);
+    }
+}
