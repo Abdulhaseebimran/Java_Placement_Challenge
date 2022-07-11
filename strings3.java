e118ac70227a8401387f56aa73cd7aa0e6a08a7a
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 20:57:12 +0500

    first commit

---
+++ strings3.java
@@ -0,0 +1,23 @@
+public class strings3 {
+    public static void main(String[] args) {
+        String name1 = "Abdul";
+        String name2 = "Haseeb";
+
+        // 1 s1 > s2 : +ve value
+        // 2 s1 < s2 : -ve value
+        // 3 s1 == s2 : 0
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not equal");
+        }
+     
+        
+    //     if(name1 == name2){
+    //         System.out.println("String are equal");
+    //     } else {
+    //         System.out.println("String are not equal");
+    //     }
+    }
+}
