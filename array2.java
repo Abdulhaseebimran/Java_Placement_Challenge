161a3724b36dd597adf6ef65881a25e7387dbe27
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 18:49:38 +0500

    first commit

---
+++ array2.java
@@ -0,0 +1,9 @@
+public class array2 {
+    public static void main(String[] args) {
+        int marks[] = {97, 98, 99};
+
+        for(int i=0; i<=3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
