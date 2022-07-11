494ced528757c970e32f6f9868d4d5bb8e48a427
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 20:58:30 +0500

    first commit

---
+++ string6.java
@@ -0,0 +1,7 @@
+public class string6 {
+    public static void main(String[] args) {
+        int number = 123456;
+        String str = Integer.toString(number);
+        System.out.println(str);
+    }
+}
