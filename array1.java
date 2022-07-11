b6a6dfa491ba0431796eefb62d27e84dbcc3d5dc
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 18:49:22 +0500

    first commit

---
+++ array1.java
@@ -0,0 +1,16 @@
+public class array1 {
+    public static void main(String[] args) {
+        int [] marks = new int [3];
+        marks[0] = 98; // phy
+        marks[1] = 99; // chem 
+        marks[2] = 97; // comp
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        
+        for(int i=0; i<=3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
