4283117afebadea07dd9332dab7d14808c2b0e45
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-11 20:57:52 +0500

    first commit

---
+++ strings5.java
@@ -0,0 +1,9 @@
+public class strings5 {
+    public static void main(String[] args) {
+        // ye method string ko integear me convert karta hey
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+
+    }
+}
