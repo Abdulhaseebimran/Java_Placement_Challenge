c6e14641146aaf2c5e29d776fa71a743fd124576
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-12 12:24:26 +0500

    first commit

---
+++ stringbuilber4.java
@@ -0,0 +1,17 @@
+public class stringbuilber4 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("HELLO");
+        
+        for(int i=0; i<sb.length() / 2; i++){
+            int front = i;
+            int back = sb.length() - 1 -i; // 5 - 1 - 0 = 4;
+            
+            char frontchar = sb.charAt(front);
+            char backchar = sb.charAt(back);
+            
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+    }
+}
