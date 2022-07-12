e45835b79f04515a824e3b69ecd6839754d5e247
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-12 12:23:34 +0500

    first commit

---
+++ stringbuilder1.java
@@ -0,0 +1,13 @@
+public class stringbuilder1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+
+        // char at index 0
+        System.out.println(sb.charAt(0));
+
+        // set char at index 0
+        // sb.setCharAt(0, "P");
+        // System.out.println(sb);
+    }
+}
