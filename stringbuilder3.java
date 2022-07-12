84ea4973aff9ee7bebec994bb473587b9b54aa4a
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-07-12 12:24:11 +0500

    first commit

---
+++ stringbuilder3.java
@@ -0,0 +1,12 @@
+public class stringbuilder3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("H");
+        sb.append("A");
+        sb.append("S");
+        sb.append("E");
+        sb.append("E");
+        sb.append("B");
+
+        System.out.println(sb);
+    }
+}
