f84d809017410e3dd0f356a4f0ef749d845b06fa
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-10-24 17:05:12 +0500

    first commit

---
+++ sorting_bubble.java
@@ -0,0 +1,25 @@
+public class sorting_bubble {
+    public static void Print_array(int arr[]){
+      for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i]+ " ");
+      }
+      System.out.println();
+    } 
+    public static void main(String[] args) {
+        int arr [] = {7, 8, 9, 3, 2, 1};
+
+        // bubble sort
+        for(int i=0; i<arr.length-1; i++ ){
+            for(int j=0; j<arr.length-i-1; j++){
+                // condition
+                if (arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] =  arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        Print_array(arr);
+    }
+}
